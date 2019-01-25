bdf406dfdfad078142903ebab86fafcacd3ff7c7
Matt Palmer <mpalmer@hezmatt.org>
2019-01-25 16:47:28 +1100

    Add a pnow wrapper

--- manifests/git.pp
+++ manifests/git.pp
@@ -1,8 +1,13 @@
+<<<<<<< HEAD
 class mlpuppet::git(
 		$future_parser = false,
 ) {
-	file { "/etc/puppet/repo/hooks/update":
-		content => template("mlpuppet/etc/puppet/repo/hooks/update"),
-		mode    => "0555";
+	file {
+		"/etc/puppet/repo/hooks/update":
+			content => template("mlpuppet/etc/puppet/repo/hooks/update"),
+			mode    => "0555";
+		"/usr/local/bin/pnow":
+			content => template("mlpuppet/usr/local/bin/pnow"),
+			mode    => "0555";
 	}
 }
